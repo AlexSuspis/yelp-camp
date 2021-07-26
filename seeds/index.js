1647b286adf9e303988a563e454d29aa19c3a73f
Alexandre Suspiro <alex@Alexandres-Air.Home>
2021-07-26 18:47:44 +0100

    reseeded atlas db with mock data. required the dotenv module in seeds/index.js so I can access the .env file and populate the atlas database by accessing its url.

--- seeds/index.js
+++ seeds/index.js
@@ -3,7 +3,14 @@ const Campground = require('../models/campground');
 const cities = require('./cities');
 const { places, descriptors } = require('./seedHelpers')
 
+
+//so we can access the .env file
+require("dotenv").config();
+
 const dbUrl = process.env.DB_URL || 'mongodb://localhost:27017/campgrounds';
+console.log(dbUrl);
+
+
 
 mongoose.connect(dbUrl, { useNewUrlParser: true, useUnifiedTopology: true })
 	.then(() => console.log('Connected to database!'))
